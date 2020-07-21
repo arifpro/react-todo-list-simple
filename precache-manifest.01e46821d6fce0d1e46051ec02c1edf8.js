460f346b489f175ba71808a69543c6b189e7e2b7
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-07-21 22:07:33 +0800

    Updates

---
+++ precache-manifest.01e46821d6fce0d1e46051ec02c1edf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbc2523576e27cf5efccee7f05e4716",
+    "url": "/react-todo-list-simple/index.html"
+  },
+  {
+    "revision": "6c193c54cafdb2836ed4",
+    "url": "/react-todo-list-simple/static/css/main.7bbabe36.chunk.css"
+  },
+  {
+    "revision": "80bf2f0d5f17c6ffbdb5",
+    "url": "/react-todo-list-simple/static/js/2.e43468ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list-simple/static/js/2.e43468ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c193c54cafdb2836ed4",
+    "url": "/react-todo-list-simple/static/js/main.7373199a.chunk.js"
+  },
+  {
+    "revision": "f907338649223c2236ea",
+    "url": "/react-todo-list-simple/static/js/runtime-main.e1ce7cd3.js"
+  }
+]);
